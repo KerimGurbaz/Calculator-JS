f61da0d31ec6948e12d93df449a8ab995a9b2963
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-09-24 00:14:58 +0200

    updated

--- app.js
+++ app.js
@@ -22,30 +22,40 @@ calculator.addEventListener("click", (e)=>{
     } else if(e.target.classList.contains("operator")){
         console.log(e.target.innerHTML)
         opt.push(e.target.innerHTML)
-        previous.innerHTML +=  current.innerHTML + `${e.target.innerHTML}`
+        previous.innerHTML +=  current.innerHTML + ` ${e.target.innerHTML}`
         current.innerHTML =""
     }
       else if(e.target.classList.contains("equal")){
         console.log("esittir");
 
         if(previous.innerHTML.slice(-1) == "+" ){
-            current.innerHTML= parseInt(previous.innerHTML.slice(0,-1)) + parseInt(current.innerHTML)
+            current.innerHTML= parseFloat(previous.innerHTML.slice(0,-1)) + parseFloat(current.innerHTML)
         }
         else if(previous.innerHTML.slice(-1) == "-"){
-            current.innerHTML= parseInt(previous.innerHTML.slice(0,-1)) - parseInt(current.innerHTML)
+            current.innerHTML= parseFloat(previous.innerHTML.slice(0,-1)) - parseFloat(current.innerHTML)
         }
         else if(previous.innerHTML.slice(-1) == "x"){
-            current.innerHTML= parseInt(previous.innerHTML.slice(0,-1)) * parseInt(current.innerHTML)
+            current.innerHTML= parseFloat(previous.innerHTML.slice(0,-1)) * parseFloat(current.innerHTML)
         }
         else if(previous.innerHTML.slice(-1) == "÷"){
-            current.innerHTML= parseInt(previous.innerHTML.slice(0,-1)) / parseInt(current.innerHTML)
+            current.innerHTML= parseFloat(previous.innerHTML.slice(0,-1)) / parseFloat(current.innerHTML)
         }
+       
+
+        previous.innerHTML =""
 
     }
     else if(e.target.classList.contains("ac")){
         current.innerHTML =""
         previous.innerHTML =""
     }
+    else if(e.target.classList.contains("percent")){
+        current.innerHTML = parseFloat(current.innerText / 100)
+    }
+
+
+
+
     else if(e.target.classList.contains("pm")){
         if(current.innerHTML > 0){
             current.innerHTML ="-".concat(current.innerHTML)
